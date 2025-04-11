7e45e3820902e62daf17114a2f3dc72e89480172
Pedro Oliveira <pedro.barbosa2730@gmail.com>
2025-04-11 14:01:37 -0300

    Update: Estudos sobre operadores de associação

---
+++ DIO/operadoresdeassociacao.py
@@ -0,0 +1,29 @@
+#Operadores de associação
+"""In: Retorna True se o valor estiver presente na sequência"""
+"""Not in: Retorna False se o valor estiver presente na sequência ou True em sua ausência"""
+
+palavra = "mamão"
+
+print ("a" in palavra)
+
+print ("a" not in palavra)
+
+cores = ('vermelho', 'azul', 'verde')
+
+print  ("azul" in cores)
+
+"""Operadores de assoiciação com dicionário"""
+"""Caso você tente procurar um valor em um dicionário 
+ele sempre irá prcurar o valor nas chaves como o exemplo abaixo:
+"""
+aluno = {
+    "nome": "Pedro",
+    "idade": 22,
+    "curso": "Engenharia"
+}
+
+print ("Pedro" in aluno )
+# Retorna False, porque "Pedro" é um valor, não uma chave.
+
+#Caso eu queria procuar o valor de uma chave devo executar 
+print ("Pedro" in aluno.values() )
